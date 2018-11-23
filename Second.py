1b50790b6c6d380279242a75eaaca1ac35f6b3fa
phys-engineer <haimarat@yandex.ru>
2018-11-23 17:47:04 +0300

    class mod

--- Second.py
+++ Second.py
@@ -1,34 +1,31 @@
-import requests
-from time import sleep
-url = "https://api.telegram.org/bot731108627:AAEH5J9XYufKrhLRFXS9VURNU7ff2Pnq4o8/"
+import requests  
+import datetime
 
+class BotHandler:
 
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
 
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
+    def get_last_update(self):
+        get_result = self.get_updates()
 
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
 
-def main():  
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)       
-
-if __name__ == '__main__':  
-    main()
+        return last_update
