53f6ca9d7c27ea1f6d2b12cdc025e391b0a8e1c4
phys-engineer <haimarat@yandex.ru>
2018-11-23 18:10:29 +0300

    Update Second.py

--- Second.py
+++ Second.py
@@ -1,34 +1,12 @@
-import requests
-from time import sleep
-url = "https://api.telegram.org/bot731108627:AAEH5J9XYufKrhLRFXS9VURNU7ff2Pnq4o8/"
+# -*- coding: utf-8 -*-
+import config
+import telebot
 
+bot = telebot.TeleBot(config.token)
 
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
-
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
-
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
-
-def main():  
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)       
-
-if __name__ == '__main__':  
-    main()
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
