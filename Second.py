09c794c0c21aede88ead2775fff66bfd18ebb7a5
phys-engineer <haimarat@yandex.ru>
2018-11-23 18:16:07 +0300

    Update Second.py

--- Second.py
+++ Second.py
@@ -6,8 +6,8 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    mes = message.text*2
-    bot.send_message(message.chat.id, message.text)
+    mes1 = message.text*2 + ' твой ответ'
+    bot.send_message(message.chat.id, mes1)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
