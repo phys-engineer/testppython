a76c1d7fa065c83f75b2b07e3dceb754588e0e4d
phys-engineer <haimarat@yandex.ru>
2018-11-23 18:23:11 +0300

    Update Second.py

--- Second.py
+++ Second.py
@@ -4,6 +4,15 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+    
+    
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     mes1 = message.text*2 + ' твой ответ'
